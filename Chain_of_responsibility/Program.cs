8a4daa6151b4c2ee8a54233473d9aa381a962607
Nester <kyrydyndyndynkrdynkrdyna@gmail.com>
2024-11-25 14:01:39 +0700

    Добавьте файлы проекта.

---
+++ Chain_of_responsibility/Program.cs
@@ -0,0 +1,36 @@
+﻿Link a = new First(new Second());
+Console.WriteLine(a.Action("A"));
+Console.WriteLine(a.Action("B"));
+try { Console.WriteLine(a.Action("C")); }
+catch { Console.WriteLine("Error!"); }
+
+a = new First(new Second(new Last()));
+Console.WriteLine(a.Action("C"));
+
+
+
+
+abstract class Link
+{
+    protected Link next;
+    public Link(Link next) { this.next = next; }
+    abstract public string Action(string action);
+}
+
+
+class First(Link next = null) : Link(next)
+{
+    override public string Action(string action) { if (action.Equals("A")) return "B"; else return next.Action(action); }
+}
+
+
+class Second(Link next = null) : Link(next)
+{
+    override public string Action(string action) { if (action.Equals("B")) return "C"; else return next.Action(action); }
+}
+
+
+class Last(Link next = null) : Link(next)
+{
+    override public string Action(string action) { return "Error"; }
+}
